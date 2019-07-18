7f1ce1945ec9328af7c4977637819f92c410be77
crstblrdrgz <crstblrdrgz@gmail.com>
2019-07-18 21:43:02 +0200

    ok

--- calculadora.py
+++ calculadora.py
@@ -3,16 +3,17 @@ tipo_operacion = input("Que operacion quieres realizar? (multiplicar / dividir /
 
 primer_numero = (int(input("Primer numero: ")))
 segundo_numero = (int(input("Segundo numero: ")))
-
+resultado = 0
 if tipo_operacion == "MULTIPLICAR":
     resultado = primer_numero * segundo_numero
 
 elif tipo_operacion == "DIVIDIR":
     resultado = primer_numero / segundo_numero
 
-elif print("Resultado: {} ".format(resultado))tipo_operacion == "SUMAR":
+elif tipo_operacion == "SUMAR":
     resultado = primer_numero + segundo_numero
 
 elif tipo_operacion == "RESTAR":
     resultado = primer_numero - segundo_numero
 
+print("Resultado: {} ".format(resultado))
