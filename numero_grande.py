88776a56abd2a58dac446f1d18190e17c1a99597
crstblrdrgz <crstblrdrgz@gmail.com>
2019-07-22 19:08:47 +0200

    ok

---
+++ numero_grande.py
@@ -0,0 +1,9 @@
+
+
+numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 30, 20, 60, 70]
+
+for indice in range(len(numeros)):
+    numero = 0
+    if numeros[indice] > numero:
+        numero = numeros[indice]
+print(numero)
