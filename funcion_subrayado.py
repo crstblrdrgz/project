af10a9f1abfab33c71167dc58d2fd0ee972e9b9e
crstblrdrgz <crstblrdrgz@gmail.com>
2019-07-24 00:20:43 +0200

    ok

---
+++ funcion_subrayado.py
@@ -0,0 +1,9 @@
+def titulo_subrayado (texto):
+    print(texto)
+    guiones = ""
+    for letra in texto:
+        guiones += "-"
+    print(guiones)
+
+
+titulo_subrayado("Hola mundo")
---
+++ repeticion_palabra.py
@@ -0,0 +1,13 @@
+
+apariciones_palabras = {"Hola":0,
+                        "como":0,
+                        "estas":0,
+                        "amigo":0}
+
+frase = "Hola Hola como estas amigo amigo"
+
+for palabra in apariciones_palabras:
+    if "hola" in frase
+
+
+
