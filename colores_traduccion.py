1861492ea08feced72204ef4e0caa1963328969f
crstblrdrgz <crstblrdrgz@gmail.com>
2019-07-23 19:35:21 +0200

    ok

---
+++ colores_traduccion.py
@@ -0,0 +1,13 @@
+
+
+colores = {"rojo":"red",
+           "azul":"blue",
+           "amarillo":"yellow",
+           "verde":"green",
+           "morado":"purple",
+           "marron":"brown",
+           "negro":"black"}
+
+pregunta = input("¿De que color quieres ver su traduccion: ")
+
+print(colores[pregunta])
