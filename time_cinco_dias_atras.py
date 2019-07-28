d8c238e118fb1f5bfe93b2ab787e5c3a71eb1c06
crstblrdrgz <crstblrdrgz@gmail.com>
2019-07-28 17:11:43 +0200

    ok

--- time_cinco_dias_atras.py
+++ time_cinco_dias_atras.py
@@ -3,6 +3,18 @@
 
 import datetime
 
-hora = datetime.time(hour=12, minute=12)
+hoy = datetime.datetime.now()
 
-print(hora)
+cinco_dias_atras = hoy - datetime.timedelta(days=5)
+
+dia_semana ={0:"Lunes",
+            1:"Martes",
+            2:"Miercoles",
+            3:"Jueves",
+            4:"Viernes",
+            5:"Sabado",
+            6:"Domingo"}
+
+print(cinco_dias_atras.strftime("Hace cinco dias era el %d del mes %m del año %Y"))
+
+print("Y era {}".format(dia_semana[cinco_dias_atras.weekday()]))
