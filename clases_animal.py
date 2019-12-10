52453a43b948fb1604685e6b4fbd2cf0922ce2fa
crstblrdrgz <crstblrdrgz@gmail.com>
2019-12-10 18:00:00 +0100

    ok

---
+++ clases_animal.py
@@ -0,0 +1,9 @@
+
+
+class Perro:
+    def __int__(self, nombre, raza, edad):
+        self.nombre = nombre
+        self.raza = raza
+        self.edad = edad
+    def speak(self):
+        print("wof wof")
---
+++ contact.py
@@ -0,0 +1,16 @@
+from datetime import datetime
+
+import time
+class Contacto:
+    def __init__(self, nombre, apellido, numero, email, fecha_nacimiento):
+        self.nombre = nombre
+        self.apellido = apellido
+        self.numero = numero
+        self.email = email
+        self.fecha_nacimiento = datetime.strptime(fecha_nacimiento, "%d/%m/%y")
+    def get_full_name(self):
+        return self.nombre + " " + self.apellido
+    def get_age(self):
+        today = datetime.today()
+        return today.year - self.fecha_nacimiento.year - \
+               ((today.month, today.day) < (self.fecha_nacimiento.month, self.fecha_nacimiento.day))
