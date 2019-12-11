30a06646112fce078bcbb53d44d13e10f6655dae
crstblrdrgz <crstblrdrgz@gmail.com>
2019-12-11 17:09:59 +0100

    ok

--- clases_animal.py
+++ clases_animal.py
@@ -1,9 +1,14 @@
 
 
-class Perro:
-    def __int__(self, nombre, raza, edad):
-        self.nombre = nombre
-        self.raza = raza
-        self.edad = edad
+class Dog:
+    def __int__(self, name, specie, age):
+        self.name = name
+        self.specie = specie
+        self.age = age
+
     def speak(self):
         print("wof wof")
+
+
+my_dog = Dog("hodor", "mestizo", 4)
+my_dog2 =
